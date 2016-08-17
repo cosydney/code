a1b49721ab9b7f6e0117e2f1f02dd0b67f642ef4
Cyrille Labesse <cyrille@inomads.hk>
2016-08-17 09:51:25 +0100

    Rails Admin

--- admin/lafourchette_active_admin/config/routes.rb
+++ admin/lafourchette_active_admin/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/super_secret_super_admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
